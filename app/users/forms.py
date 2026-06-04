bcf0aa63915fa3d49a13f9cafebaf8ad0fb472de
Yashi Saini <yashisaini718@gmail.com>
2026-06-04 16:27:56 +0530

    empty user correction

--- app/users/forms.py
+++ app/users/forms.py
@@ -49,6 +49,13 @@ class UpdateAccountForm(FlaskForm):
 class RequestResetForm(FlaskForm):
     email=StringField('Email', validators=[DataRequired(),Email()])
     submit=SubmitField('Request Reset Password')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError(
+                'There is no account with that email. You must register first.'
+            )
 
 class ResetPasswordForm(FlaskForm):
     password=PasswordField('Password', validators=[DataRequired()])
